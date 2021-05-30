7f3aca3b11f280ee88ebb46cfda30f7e10aa4513
khoirulromadhon <khoirulromadhon1112@gmail.com>
2021-05-31 00:52:24 +0700

    Praktikum 11

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+use App\Traits\ApiResponse;
+use Facede\FlareClient\Http\Response;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exception\HttpResponseException;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+    
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiErrors(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+}
